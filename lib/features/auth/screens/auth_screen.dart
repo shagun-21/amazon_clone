87380bebe5c24a72869c17d97805c8dc443e763f
shagun <shagun2001ch@gmail.com>
2022-06-15 14:58:15 +0530

    radio button added on auth screen

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,6 +1,11 @@
 import 'package:amazon_clone/constants/global_variable.dart';
 import 'package:flutter/material.dart';
 
+enum Auth{
+  signin,
+  signup
+}
+
 
 class AuthScreen extends StatefulWidget {
   static const String routeName='/auth-screen';
@@ -11,6 +16,8 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+
+  Auth _auth=  Auth.signin;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +30,38 @@ class _AuthScreenState extends State<AuthScreen> {
             const Text("Welcome",
             style: TextStyle(fontSize: 22 ,
             fontWeight: FontWeight.w500),)
+            ,ListTile(
+              title: Text("Create account",
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),),
+              leading: Radio(
+                activeColor: GlobalVariables.secondaryColor,
+                value: Auth.signup,
+                groupValue: _auth,
+                onChanged: (Auth? val){
+                  setState(() {
+                    _auth= val!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: Text("Sign-In",
+              style: TextStyle(
+                fontWeight: FontWeight.bold
+              ),),
+              leading: Radio(
+                activeColor: GlobalVariables.secondaryColor,
+                value: Auth.signup,
+                groupValue: _auth,
+                onChanged: (Auth? val){
+                  setState(() {
+                    _auth= val!;
+                  });
+                },
+              ),
+            ),
           ],
         ),
       )),
