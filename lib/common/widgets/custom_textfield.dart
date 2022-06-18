dd531f79340a0be9fa7aaf17dd740699b4fd36b8
shagun <shagun2001ch@gmail.com>
2022-06-19 00:01:53 +0530

    added

--- lib/common/widgets/custom_textfield.dart
+++ lib/common/widgets/custom_textfield.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatefulWidget {
-  const CustomTextField({ Key? key }) : super(key: key);
+  final TextEditingController controller;
+  const CustomTextField({ Key? key ,required this.controller}) : super(key: key);
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
@@ -11,6 +14,22 @@ class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black38
+          )
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black38
+          )
+        )
+      ),
+      validator: (val){
+
+      },
       
     );
   }
